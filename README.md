6354bdfc4c1466694c57478107babd7126cc9feb
vinicius-hso <viniciushso@icloud.com>
2024-02-06 10:47:10 -0300

    writing data to csv

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-# declaracoes-reader
+# declaracoes-data-extractor
+
+Aplicação destinada a extração de dados de declaraçõers de imposto de renda em arquivo PDF.
+
+## preparando o ambiente
+
+Instalar Python
+
+[Link para instalação](https://www.python.org/downloads/)
+
+Instalar Virtualenv
+
+[Link para instaação](https://pypi.org/project/virtualenv/)
+
+Criar ambiente virtual
+
+> no diretorio raiz do projeto, crie um ambiente virtual
+
+`virtualenv venv`
+
+Ativar ambiente virtual
+
+Windows: `.\venv\Scripts\activate`
+
+MacOS: `source venv/bin/activate`
+
+Instalar dependências
+
+`pip install -r requirements.txt`
+
+## executando aplicação
+
+Na raiz do projeto execute o comando `python3 main.py`
+
+Insira o caminho do diretório das declarações, um arquivo "relatorio.csv" será criado no diretório raiz da aplicação.
+
+Exemplo:
+
+> Insira o caminho do diretório de "Declarações": /Users/viniciusoliveira/Downloads/declaracoes
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import csv
 from PDFUtils import Reader
 
@@ -8,32 +9,49 @@ path = input('Insira o caminho do diretório de "Declarações": ')
 # verificar diretório
 dirs = os.listdir(path)
 
-for dir in [dirs[0]]:
+# TODO: criar csv...
+with open('./relatorio.csv', 'w') as f:
+    # create the csv writer
+    writer = csv.writer(f)
     
-    if '.' == dir[0]: continue
+    headers = {
+        "NOME": None,
+        "DATA NASCIMENTO": None,
+        "TÍTULO ELEITORAL": None,
+        "MUNICÍPIO": None,
+        "UF": None,
+        "OCUPAÇÃO": None,
+        "CPF": None
+    }
     
-    dir_path = f'{path}/{dir}'
-    
-    files = os.listdir(dir_path)
-    
-    # diretório vazio
-    if not len(files): continue
-    
-    pdfs_content = []
-    
-    for file in files:
-        if '.pdf' in file:
-            
-            # ler e extrair dados do arquivo
-            file_path = f'{dir_path}/{file}'
-            reader = Reader(file_path)
-            content = reader.extract()
-            pdfs_content.append(content)
-            
-    # TODO: criar csv...
-    with open('./relatorio', 'w') as f:
-            # create the csv writer
-            writer = csv.writer(f)
+    writer.writerow(headers)
 
+    for dir in dirs:
+        
+        if '.' == dir[0]: continue
+        
+        dir_path = f'{path}/{dir}'
+        
+        files = os.listdir(dir_path)
+        
+        # diretório vazio
+        if not len(files): continue
+        
+        pdfs_content = []
+        
+        for file in files:
+            if '.pdf' in file:
+                
+                windows_platform = 'win32' in sys.platform
+                # ler e extrair dados do arquivo
+                file_path = f'{dir_path}\{file}' if windows_platform else f'{dir_path}/{file}'
+                reader = Reader(file_path)
+                content = reader.extract()
+                pdfs_content.append(content)
+                
+        for c in pdfs_content:
             # write a row to the csv file
-            writer.writerow(content)
+            writer.writerow(c.values())
+                
+                
+# /Users/viniciusoliveira/Downloads/declaracoes
