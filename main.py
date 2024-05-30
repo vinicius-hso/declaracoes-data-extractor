4d43c8e68ad90bb6941e5276802ef06b0caae4da
vinicius-hso <viniciushso@icloud.com>
2024-05-30 17:54:44 -0300

    refactor

--- main.py
+++ main.py
@@ -3,55 +3,82 @@ import sys
 import csv
 from PDFUtils import Reader
 
-# ler entrada do usuário path destino dos arquivos
-path = input('Insira o caminho do diretório de "Declarações": ')
+input_dir_path = input('Insira o caminho do diretório de "Declarações": ')
+output_dir_path = input('Insira o caminho de diretõrio para salvar o arvquido de relatório: ')
 
-# verificar diretório
-dirs = os.listdir(path)
+# input_dir_path = "C:\\Users\\vinicius\\Downloads\\declaracoes\\declaracoes"
+# output_dir_path = input_dir_path
 
-# TODO: criar csv...
-with open('./relatorio.csv', 'w') as f:
-    # create the csv writer
-    writer = csv.writer(f)
-    
-    headers = {
-        "NOME": None,
-        "DATA NASCIMENTO": None,
-        "TÍTULO ELEITORAL": None,
-        "MUNICÍPIO": None,
-        "UF": None,
-        "OCUPAÇÃO": None,
-        "CPF": None
-    }
-    
-    writer.writerow(headers)
+# C:\Users\vinicius\Downloads\declaracoes\declaracoes
 
+def get_pdf_paths(dir_path, pdfs = []):
+
+    # percorrer dirs e salvar .pdf em lista
+    pdfs_paths = []
+    dirs = os.listdir(dir_path)
+
+     # percorrer os diretórios
     for dir in dirs:
+        # ignorando arquivos invisíveis
+        if '.' == dir[0] or "." in dir : continue   
         
-        if '.' == dir[0]: continue
-        
+        # identificando Plataforma
         windows_platform = 'win32' in sys.platform
         
-        dir_path = f'{path}\\{dir}' if windows_platform else f'{path}/{dir}'
+        # montando caminho do arquivo de acordo com a plataforma
+        path = ""
+        path = f'{dir_path}\\{dir}' if windows_platform else f'{dir_path}/{dir}'
         
-        files = os.listdir(dir_path)
-        
-        # diretório vazio
-        if not len(files): continue
-        
-        pdfs_content = []
+        # # listar arquivos do diretorio
+        files = os.listdir(path)
         
         for file in files:
-            if '.pdf' in file:
-                # ler e extrair dados do arquivo
-                file_path = f'{dir_path}\\{file}' if windows_platform else f'{dir_path}/{file}'
-                reader = Reader(file_path)
-                content = reader.extract()
-                pdfs_content.append(content)
-                
-        for c in pdfs_content:
+            if ".pdf" in file:
+                file_path = f'{path}\\{file}' if windows_platform else f'{path}/{file}'
+                pdfs_paths.append(file_path)
+        
+    # print(pdfs_paths)
+    return pdfs_paths
+
+def get_pdf_content(pdf_path):
+    reader = Reader(pdf_path)
+    content = reader.extract()
+    # print(content)
+    return content 
+    
+def create_csv_report(out, headers, content):
+
+    windows_platform = 'win32' in sys.platform
+    out_file =  f'{out}\\relatorio.csv' if windows_platform else f'{out}/relatorio.csv'
+    
+
+    with open(out_file, 'w') as f:
+        # create the csv writer
+        writer = csv.writer(f)
+
+        # create headers
+        writer.writerow(headers)
+
+        for c in content:
             # write a row to the csv file
             writer.writerow(c.values())
-                
+
+HEADERS = {
+    "NOME": None,
+    "DATA NASCIMENTO": None,
+    "TÍTULO ELEITORAL": None,
+    "MUNICÍPIO": None,
+    "UF": None,
+    "OCUPAÇÃO": None,
+    "CPF": None
+}
+
+if __name__ == '__main__':
+
+    pdfs = get_pdf_paths(input_dir_path)
+    content = [get_pdf_content(pdf) for pdf in pdfs]
+
+    create_csv_report(output_dir_path, HEADERS, content)
+    print(f'Relatório criado com sucesso: `{output_dir_path}')
                 
 # /Users/viniciusoliveira/Downloads/declaracoes
